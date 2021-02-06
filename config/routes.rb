807731961be0f5f4df35826d9c82aed58d702492
Praveen Pyda <praveen.soft20@gmail.com>
2021-02-06 21:40:20 +0000

    User authentication added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  devise_for :users, path:"",path_names:{sign_up: "register",sign_in: "login",sign_out: "logout"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
